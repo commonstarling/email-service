a7b7ef64c5aa2f6ac3785be2b50996bd35831b60
Sheena Ramirez <sheenaramirez2@gmail.com>
2017-08-24 21:18:28 -0500

    Complete ability to send messages through SendGrid

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var keys = require('./apiKeys');
 var helper = require('sendgrid').mail;
-var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+var sg = require('sendgrid')(keys.SENDGRID_API_KEY);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(3000);
 console.log('Server listening on port 3000');
 
 app.post('/sendEmail', function(req, res) {
-  var fromEmail = new helper.Email(req.from);
-  var toEmail = new helper.Email(req.to);
-  var subject = req.subject;
-  var content = new helper.Content('text/plain', req.message);
+  var fromEmail = new helper.Email(req.body.from);
+  var toEmail = new helper.Email(req.body.to);
+  var subject = req.body.subject;
+  var content = new helper.Content('text/plain', req.body.message);
   var mail = new helper.Mail(fromEmail, subject, toEmail, content);
   var request = sg.emptyRequest({
     method: 'POST',
@@ -19,11 +24,12 @@ app.post('/sendEmail', function(req, res) {
   });
 
   sg.API(request, function (error, response) {
+    //console.log('===================sending')
     if (error) {
-      console.log('Error response received');
+      console.log('===================sending', error.response.body.errors)
+      res.sendStatus(404);
+    } else {
+      res.status(200).send(response)
     }
-    console.log(response.statusCode);
-    console.log(response.body);
-    console.log(response.headers);
   });
 });
